e6953651bb74800c8af2373236040fc12d316975
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2023-03-08 09:16:50 +0100

    update

--- arc-connection/bash.sh
+++ arc-connection/bash.sh
@@ -76,6 +76,31 @@ echo "Connecting the cluster to Azure Arc"
 echo "az connectedk8s connect --name $eksclustername --resource-group $resourcegroupnameforarc --location $location --custom-locations-oid 22cfa2da-1491-4abc-adb3-c31c8c74cefa"
 az connectedk8s connect --name $eksclustername --resource-group $resourcegroupnameforarc --location $location --custom-locations-oid "22cfa2da-1491-4abc-adb3-c31c8c74cefa"
 
+echo "Create Service Account to see K8s resources"
+echo "-------------------------------------------"
+# 1
+kubectl create serviceaccount arc-ui-user
+
+# 2
+kubectl create clusterrolebinding arc-ui-user-binding --clusterrole cluster-admin --serviceaccount default:arc-ui-user
+
+# 3
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: arc-ui-user-secret
+  annotations:
+    kubernetes.io/service-account.name: arc-ui-user
+type: kubernetes.io/service-account-token
+EOF
+
+# 4
+TOKEN=$(kubectl get secret arc-ui-user-secret -o jsonpath='{$.data.token}' | base64 -d | sed 's/$/\n/g')
+echo $TOKEN
+echo $TOKEN > token.txt
+
+
 # # Add the official stable repo
 # helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 # helm repo update
